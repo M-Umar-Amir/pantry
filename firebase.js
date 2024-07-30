47751b0f70260eac36ab62afd4fb000eb359c6f0
Muhammad Umar Amir <125129208+M-Umar-Amir@users.noreply.github.com>
2024-07-30 14:15:37 +0500

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,12 +1,8 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-import { getFirestore } from "firebase/firestore";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { useEffect, useState } from 'react';
+import { initializeApp } from 'firebase/app';
+import { getAnalytics } from 'firebase/analytics';
+import { getFirestore } from 'firebase/firestore';
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyDahZyOHPoIVQajCAFXQ4YpU17GfLUq_jE",
   authDomain: "pantryapp-65381.firebaseapp.com",
@@ -17,11 +13,14 @@ const firebaseConfig = {
   measurementId: "G-8DQMZEB8Y1"
 };
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
-const firestore = getFirestore(app);
-export {
-    app,
-    firestore
+let app;
+let firestore;
+if (typeof window !== 'undefined') {
+  app = initializeApp(firebaseConfig);
+  if ('measurementId' in firebaseConfig) {
+    getAnalytics(app);
+  }
+  firestore = getFirestore(app);
 }
+
+export { app, firestore };
